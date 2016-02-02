7307899b1011d5c4922e9b6f58f59ed7f002fd5f
Thomas Poncet <thomas.poncet@ensimag.grenoble-inp.fr>
2016-02-02 15:34:36 +0100

    Adding voteT1 and voteT2 management

---
+++ client/js/dataProvider.js
@@ -0,0 +1,41 @@
+angular.module('ProjectOpenData')
+.factory('dataProvider', ['$http', function(http) {
+    var dataProvider = {};
+
+
+    /**
+     * Vote first tour management
+     **/
+    this.voteT1 = {}
+    this.getVoteT1 = function(codeDep, codeParti) {
+        if (typeof(voteT1.codeDep) != undefined) {
+            if (typeof(voteT1.codeDep.codeParti) != undefined) {
+            } else {
+                // voteT1.codeDep.codeParti = http.get("localhost:300/getVoteT1/?codeDep="+"codeDep&codeParti="+codeParti);
+            }
+        } else {
+            voteT1.codeDep = {};
+            // voteT1.codeDep.codeParti = http.get("localhost:300/getVoteT1/?codeDep="+"codeDep&codeParti="+codeParti);
+        }
+        return voteT1.codeDep.codeParti;
+    };
+
+    /**
+     * Vote second tour management
+     **/
+     this.voteT2 = {}
+     this.getVoteT2 = function(codeDep, codeParti) {
+         if (typeof(voteT2.codeDep) != undefined) {
+             if (typeof(voteT2.codeDep.codeParti) != undefined) {
+             } else {
+                 // voteT2.codeDep.codeParti = http.get("localhost:300/getVoteT2/?codeDep="+"codeDep&codeParti="+codeParti);
+             }
+         } else {
+             voteT2.codeDep = {};
+             // voteT2.codeDep.codeParti = http.get("localhost:300/getVoteT2/?codeDep="+"codeDep&codeParti="+codeParti);
+         }
+         return voteT2.codeDep.codeParti;
+     };
+
+
+});
