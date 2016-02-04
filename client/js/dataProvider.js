4ce8a365e3286e9de0306e2f83ec2e93fc7cd9f4
Thomas Poncet <thomas.poncet@ensimag.grenoble-inp.fr>
2016-02-04 20:30:45 +0100

    Gestion cas limit allCorelations

--- client/js/dataProvider.js
+++ client/js/dataProvider.js
@@ -73,7 +73,7 @@ angular.module('ProjectOpenData')
     dataProvider.allCorrelations = {};
     dataProvider.getAllCorrelations = function(tour, codesPartiListe, datasetId, callback){
         if(codesPartiListe.length == 0 || typeof(datasetId)=="undefined"){
-            callback([]);
+            callback(null);
         }
         if (typeof(dataProvider.allCorrelations[tour]) != "undefined"){
             if (typeof(dataProvider.allCorrelations[tour][JSON.stringify(codesPartiListe)]) != "undefined") {
